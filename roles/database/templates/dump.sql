e9fbda4d5d24e99fb7252041cdc824d5ad418705
craig01 <craigkbrown@gmail.com>
2016-02-12 22:17:39 +1100

    Adding database and table creation

---
+++ roles/database/templates/dump.sql
@@ -0,0 +1,31 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pressure`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `readings`
+--
+
+CREATE TABLE IF NOT EXISTS `readings` (
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `systolic` int(3) NOT NULL,
+  `diastolic` int(3) NOT NULL,
+  `heartrate` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
