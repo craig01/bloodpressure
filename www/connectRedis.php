61d9ddfc73d479d369567ce52747e6007329b74f
craig01 <craigkbrown@gmail.com>
2016-04-13 08:28:44 +1000

    New initial pages

--- www/connectRedis.php
+++ www/connectRedis.php
@@ -1,23 +1,25 @@
 <?php
-require "/tmp/predis/autoload.php";
+include('/tmp/predis/predis-1.0');
+
+require '/tmp/predis/predis-1.0/autoload.php';
 PredisAutoloader::register();
 
 try {
-	$redis = new PredisClient();
-
-	// This connection is for a remote server
-	/*
-		$redis = new PredisClient(array(
-		    "scheme" => "tcp",
-		    "host" => "153.202.124.2",
-		    "port" => 6379
-		));
-	*/
+    $redis = new PredisClient();
+/*
+    $redis = new PredisClient(array(
+        "scheme" => "tcp",
+        "host" => "127.0.0.1",
+        "port" => 6379));
+*/
+    echo "Successfully connected to Redis";
 }
 catch (Exception $e) {
-	die($e->getMessage());
+    echo "Couldn't connected to Redis";
+    echo $e->getMessage();
+}
 
-$redis->set(';message';, ';Hello world';);
+$redis->set("message", "Hello world");
 
 // gets the value of message
 $value = $redis->get('message');
@@ -25,6 +27,4 @@ $value = $redis->get('message');
 // Hello world
 print($value);
 
-echo ($redis->exists('message')) ? "Oui" : "please populate the message key";
-
-}
+?>
